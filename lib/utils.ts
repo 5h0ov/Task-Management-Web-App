bdda834dc5cdc352a31c3764c91fd88bdfeea10d
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2025-06-17 19:15:23 +0530

    robots and sitemap added

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function absoluteUrl(path: string) {
+  return new URL(path, process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000').toString();
+}
